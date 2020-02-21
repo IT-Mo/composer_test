3550453922d409c5e20ddb75d6a3bba6505de14a
莫泞衡 <1173874670@qq.com>
2020-02-21 11:27:59 +0800

    第一次提交文件到git

---
+++ code/controller/comment.php
@@ -0,0 +1,9 @@
+<?php
+namespace code\controller;
+
+class comment{
+
+	public static function show_info(){
+		echo "hello world!";
+	}
+}
---
+++ code/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use  code\controller\comment;
+comment::show_info();
